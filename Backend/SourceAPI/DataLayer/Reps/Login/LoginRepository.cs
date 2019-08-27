98b6f39a18ece8fdc94191f0632479dcab1cf6cb
Alireza Razinejad <a.razinejad@gmail.com>
2019-08-27 16:20:15 +0430

    Update Debug Structure Attendance Controller

--- Backend/SourceAPI/DataLayer/Reps/Login/LoginRepository.cs
+++ Backend/SourceAPI/DataLayer/Reps/Login/LoginRepository.cs
@@ -45,21 +45,27 @@ namespace AttendanceApi.Reps
         }
 
         public async Task<RepResult<PersonnelVM>> Attend(string nationalCole)    {
+            Console.Write("\n################NATIONAL CODE################\n" + nationalCole + "\n");
+            Console.Write("\n################ Contect ################\n" + dbContext + "\n");
             if (dbContext != null) {
+                Console.Write("\n################Got into the first if################\n");
                 var query = from prs in dbContext.Personnels join user in dbContext.Users on prs.PersonnelId equals user.PersonnelId
                     where prs.NationalCode == nationalCole select prs;
 
             if (await query.AnyAsync()) {
+                Console.Write("\n################Got into the second if################\n");
                 var obj = await query.Include(c => c.Users).SingleOrDefaultAsync();
                 PersonnelRepository.LoadUser(obj);
                 return new RepResult<PersonnelVM> { Successed = true };
             } else {
+                Console.Write("\n################Got into the second else of second if################\n");
                 return new RepResult<PersonnelVM> {
                     Successed = false,
                 };
             }
 
             }
+            Console.Write("\n################Not Got to the first if################\n" + nationalCole + "\n");
             return new RepResult<PersonnelVM>();
         }
     }
